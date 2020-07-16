e0b04f50f350009731caa148b86e8fc2d271ab80
davien21 <chidiebereekennia@gmail.com>
2020-07-16 23:26:08 +0100

    changed default checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,51 @@ function addTask (task) {
 	increaseCounter(task_counter);
 	increaseCounter(pending_counter);
 }
+function addTaskToList (task) {
+	let task_list_item = 
+		`<li class="col-12 d-flex flex-wrap list-group-item paper-box-shadow">
+			<div class='col px-0'>
+				<span class='task'>${task}</span>
+			</div>
+			<div class='ml-auto row'>
+				<div class='' onclick="toggleCrossTask(this);">
+					<img class='px-0 btn-svg check-img' src='./assets/imgs/svgs/checkbox-2.svg' title='Check This Task'>
+				</div>
+				<div class='px-2' onclick="editTask(this);">
+					<img class='px-0 btn-svg' src='./assets/imgs/svgs/edit.svg' title='Edit This Task'>
+				</div>
+				<div class='' onclick="deleteTask(this);">
+					<img class='px-0 btn-svg' src='./assets/imgs/svgs/delete-2.svg' title='Delete This Task'>
+				</div>
+			</div>
+		</li>`;
+	task_list.innerHTML+=task_list_item;
+}
+let addTaskError = (task) => {
+	if (task.length === 0) {
+		return 'A Task cannot be empty';
+	}else if (task.length < 3) {
+		return 'This Task is too short';
+	}else {
+		return false;
+	}
+}
+let formatted_text = (text) => {
+	let output = text.trim().toLowerCase();
+	output = output.charAt(0).toUpperCase() + output.slice(1);
+	return output;
+}
+
+console.log(add_btn)
+console.log(task_input)
+task_form.onsubmit = () => {
+	event.preventDefault();
+	let task = task_input.value;
+	handleAddTask(task); 
+}
+
+//Functions for crossing tasks:
+
 function toggleCrossTask (btn) {
 	let task_li = btn.closest('li');
 	let task_text = task_li.querySelector('span.task') ;
@@ -47,7 +92,7 @@ function crossTask (img,text,li) {
 
 }
 function uncrossTask (img,text,li) {
-	img.src = './assets/imgs/svgs/check-3.svg';
+	img.src = './assets/imgs/svgs/checkbox-2.svg';
 	img.title = 'Check This Task';
 	text.classList.remove('cross-text');
 	li.classList.remove('crossed');
@@ -79,47 +124,5 @@ function increaseCounter (element) {
 function decreaseCounter (element) {
 	element.innerText--;
 }
-function addTaskToList (task) {
-	let task_list_item = 
-		`<li class="col-12 d-flex flex-wrap list-group-item paper-box-shadow">
-			<div class='col px-0'>
-				<span class='task'>${task}</span>
-			</div>
-			<div class='ml-auto row'>
-				<div class='' onclick="toggleCrossTask(this);">
-					<img class='px-0 btn-svg check-img' src='./assets/imgs/svgs/check-3.svg' title='Check This Task'>
-				</div>
-				<div class='px-2' onclick="editTask(this);">
-					<img class='px-0 btn-svg' src='./assets/imgs/svgs/edit.svg' title='Edit This Task'>
-				</div>
-				<div class='' onclick="deleteTask(this);">
-					<img class='px-0 btn-svg' src='./assets/imgs/svgs/delete-2.svg' title='Delete This Task'>
-				</div>
-			</div>
-		</li>`;
-	task_list.innerHTML+=task_list_item;
-}
-let addTaskError = (task) => {
-	if (task.length === 0) {
-		return 'A Task cannot be empty';
-	}else if (task.length < 3) {
-		return 'This Task is too short';
-	}else {
-		return false;
-	}
-}
-let formatted_text = (text) => {
-	let output = text.trim().toLowerCase();
-	output = output.charAt(0).toUpperCase() + output.slice(1);
-	return output;
-}
 
-console.log(add_btn)
-console.log(task_input)
-/*Main */
-task_form.onsubmit = () => {
-	event.preventDefault();
-	let task = task_input.value;
-	handleAddTask(task); 
-}
  
