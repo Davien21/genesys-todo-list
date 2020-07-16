5c6e22068bc73961a52127cea2b12cb3e26b781c
davien21 <chidiebereekennia@gmail.com>
2020-07-16 12:12:16 +0100

    added cross functions and UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let task_input = document.querySelector('#task-input');
 let task_form = document.querySelector('form.add-form');
 let task_list = document.querySelector('#task-display');
 let task_counter = document.querySelector('#task-count');
+let pending_counter = document.querySelector('#pending-count');
 
 //Functions for adding tasks:
 function handleAddTask(task) {
@@ -24,17 +25,59 @@ function displayAddTaskError(error) {
 
 function addTask (task) {
 	addTaskToList(formatted_text(task));
-	increaseTaskCounter();
+	increaseCounter(task_counter);
+	increaseCounter(pending_counter);
+}
+function toggleCrossTask (btn) {
+	let task_li = btn.closest('li');
+	let task_text = task_li.querySelector('span.task') ;
+	let check_img = task_li.querySelector('img.check-img');
+	if (!hasClass(task_li,'crossed')) {
+		crossTask(check_img,task_text,task_li);
+	}else {
+		uncrossTask(check_img,task_text,task_li);
+	}
+}
+function crossTask (img,text,li) {
+	img.src = './assets/imgs/svgs/checked-2.svg';
+	img.title = 'Uncheck This Task';
+	text.classList.add('cross-text');
+	li.classList.add('crossed');
+	decreaseCounter(pending_counter);
 
 }
-function crossTask (task) {
-	addTaskToList(formatted_text(task));
-	increaseTaskCounter();
+function uncrossTask (img,text,li) {
+	img.src = './assets/imgs/svgs/check-3.svg';
+	img.title = 'Check This Task';
+	text.classList.remove('cross-text');
+	li.classList.remove('crossed');
+	increaseCounter(pending_counter);
 
 }
-function increaseTaskCounter () {
-	task_counter.innerText++;
-console.log(task_list)
+let hasClass =  (element,class_name) => {
+	if (Object.values(element.classList).includes(class_name)) {
+		return true;
+	}else {
+		return false;
+	}
+}
+function editTask (btn) {
+	console.log(btn.closest('li'))
+
+}
+function deleteTask (btn) {
+	console.log(btn.closest('li'))
+
+}
+function displayCross() {
+	
+}
+
+function increaseCounter (element) {
+	element.innerText++;
+}
+function decreaseCounter (element) {
+	element.innerText--;
 }
 function addTaskToList (task) {
 	let task_list_item = 
@@ -43,13 +86,13 @@ function addTaskToList (task) {
 				<span class='task'>${task}</span>
 			</div>
 			<div class='ml-auto row'>
-				<div class=''>
-					<img class='px-0 btn-svg' src='./assets/imgs/svgs/check-3.svg' title='Check This Task'>
+				<div class='' onclick="toggleCrossTask(this);">
+					<img class='px-0 btn-svg check-img' src='./assets/imgs/svgs/check-3.svg' title='Check This Task'>
 				</div>
-				<div class='px-2'>
+				<div class='px-2' onclick="editTask(this);">
 					<img class='px-0 btn-svg' src='./assets/imgs/svgs/edit.svg' title='Edit This Task'>
 				</div>
-				<div class=''>
+				<div class='' onclick="deleteTask(this);">
 					<img class='px-0 btn-svg' src='./assets/imgs/svgs/delete-2.svg' title='Delete This Task'>
 				</div>
 			</div>
